f06eb5458a724a62d062749616558b3f894742a2
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-07-20 10:56:22 -0500

    Checks to see if input is a number. Gives option to change increment

--- for.php
+++ for.php
@@ -2,9 +2,38 @@
 
 fwrite(STDOUT, "What is the starting number? ");
 $startingNumber = trim(fgets(STDIN));
+if (! is_numeric($startingNumber)) {
+	echo "You must enter a number!".PHP_EOL;
+	exit;
+}
+
 fwrite(STDOUT, "What is the ending number? ");
 $endingNumber = trim(fgets(STDIN));
+if (! is_numeric($startingNumber)) {
+	echo "You must enter a number!".PHP_EOL;
+	exit;
+}
 
-for($i = $startingNumber; $i < $endingNumber; $i++){
+fwrite(STDOUT, "What is the increment? ");
+$increment = trim(fgets(STDIN));
+if (! is_numeric($startingNumber)) {
+	echo "You must enter a number!".PHP_EOL;
+	exit;
+}
+ 
+
+
+if ($increment == ""){
+	$increment = 1;
+}
+
+// if (!is_numeric($startingNumber) \\ !is_numeric($endingNumber) || !is_numeric($increment)){
+// 	echo "Inputs aren\'t numeric! Try again." . PHP_EOL;
+// 	die;
+// }
+
+for($i = $startingNumber; $i < $endingNumber; $i+= $increment){
 	echo $i . PHP_EOL;
 }
+
+
