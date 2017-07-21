df562b7234e528dc9501f1a680a3457a42bc2ae7
Emmanuelle Taylor Maher <emmanuelle.t.maher@gmail.com>
2017-07-21 11:30:48 -0500

    Updated loop to show books that have more than 300 pages

--- foreach_books.php
+++ foreach_books.php
@@ -29,7 +29,7 @@ $books = array(
 //Update your loop to only show books that were written after 1950.
 
 foreach ($books as $key => $book) {
-	if ($book["published"] > 1950) {
+	if ($book["published"] > 1950 && $book["pages"] > 300) {
 	echo "The book's title is {$key}".PHP_EOL;
 		foreach ($book as $key => $stats) {
 			echo "{$key}: {$stats}".PHP_EOL;	
